34ea52d8429c5516531c73cde5000c6f82066567
dulcelene machado <dudutola@gmail.com>
2024-01-20 10:21:35 +0100

    Added a new class filterbar, with 4 span inside, each containing a different category label Added style to the filterbar and span, and hover to change properties Added a logic in main.js to filter the photos on the page, to only show Objets, Appartements, Hôtels & restaurants or Tous, and did this using event listener, and switch

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -6,3 +6,35 @@ const works = await apiWorks.json();
 console.log(works)
 
 generateWorks(works);
+
+const filterButtons = document.querySelectorAll(".filterbar span");
+filterButtons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    console.log(event)
+
+    console.log(button);
+
+    let filterTerm = button.textContent.trim();
+
+    switch (filterTerm) {
+      case "Objets":
+        const worksObjets = works.filter((work) => work.category.name === "Objets");
+        generateWorks(worksObjets)
+        break;
+
+      case "Appartements":
+        const worksAppartements = works.filter((work) => work.category.name === "Appartements");
+        generateWorks(worksAppartements)
+        break;
+
+      case "Hôtels & restaurants":
+        const worksHotelsResto = works.filter((work) => work.category.name === "Hotels & restaurants");
+        generateWorks(worksHotelsResto)
+        break;
+
+      default:
+        generateWorks(works)
+        break;
+    }
+  });
+})
