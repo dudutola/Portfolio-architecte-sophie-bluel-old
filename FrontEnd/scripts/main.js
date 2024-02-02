c7a965eea7730cd716a973beef70e6664718db65
dulcelene machado <dudutola@gmail.com>
2024-02-02 11:28:11 +0100

    Update the localStorage name to userData Removed the function verifyUserLogin from main.js

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -12,72 +12,66 @@ generateIntroduction();
 generatePortfolio(works);
 generateContact();
 
-// Declare function users view
-function verifyIfUserLogin() {
-  let userLoginInfos = window.localStorage.getItem("portfolio");
-  console.log(userLoginInfos)
+// users view
+let userLoginInfos = window.localStorage.getItem("userData");
 
-  if (userLoginInfos) {
-    const marginHeader = document.querySelector("header");
-    marginHeader.style.margin = "100px 0 50px";
+if (userLoginInfos) {
+  //
+  const marginHeader = document.querySelector("header");
+  marginHeader.style.margin = "100px 0 50px";
 
-    // Create mode édition
-    const edition = document.createElement("div");
-    edition.className = "mode edition";
-    edition.innerHTML = `
+  // Create mode édition
+  const edition = document.createElement("div");
+  edition.className = "mode edition";
+  edition.innerHTML = `
+    <i class="fa-regular fa-pen-to-square"></i>
+    <p>Mode édition</p>`;
+  const body = document.querySelector("body");
+  body.insertAdjacentElement("afterbegin", edition)
+
+  // Create li logout
+  const logout = document.createElement("li");
+  logout.innerText = "logout";
+  logout.className = "logout";
+
+  const ul = document.querySelector("nav ul")
+  const listItems = ul.getElementsByTagName("li");
+  console.log(listItems)
+  const lastItem = listItems[listItems.length - 1];
+  lastItem.insertAdjacentElement("beforebegin", logout);
+
+  logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.localStorage.removeItem("userData");
+
+    location.reload();
+  });
+
+  // Not display login
+  const loginLi = ul.querySelector("li:nth-child(3)");
+  loginLi.style.display = "none";
+
+  // Remove default h2
+  const mesProjets = document.getElementById("projets");
+  mesProjets.style.display = "none";
+
+  // Create modifier
+  const mode = document.createElement("div");
+  mode.className = "mode";
+  mode.innerHTML = `
+    <h2>Mes projets</h2>
+    <div class="modify">
       <i class="fa-regular fa-pen-to-square"></i>
-      <p>Mode édition</p>`;
-    const body = document.querySelector("body");
-    body.insertAdjacentElement("afterbegin", edition)
+      <p>modifier</p>
+    </div>`;
 
-    // Create li logout
-    const logout = document.createElement("li");
-    logout.innerText = "logout";
-    logout.className = "logout";
+  const sectionPortfolio = document.getElementById("portfolio");
+  sectionPortfolio.insertAdjacentElement("beforebegin", mode);
 
-    const ul = document.querySelector("nav ul")
-    const listItems = ul.getElementsByTagName("li");
-    console.log(listItems)
-    const lastItem = listItems[listItems.length - 1];
-    lastItem.insertAdjacentElement("beforebegin", logout);
+  const modifyButton = document.querySelector(".modify");
+  modifyButton.addEventListener("click", () => generateModal(works));
 
-    logout.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.localStorage.removeItem("portfolio");
-
-      location.reload();
-    });
-
-    // Not display login
-    const loginLi = ul.querySelector("li:nth-child(3)");
-    loginLi.style.display = "none";
-
-    // Create modifier
-    const mode = document.createElement("div");
-    mode.className = "mode";
-    mode.innerHTML = `
-      <h2>Mes projets</h2>
-      <div class="modify">
-        <i class="fa-regular fa-pen-to-square"></i>
-        <p>modifier</p>
-      </div>`;
-
-    const mesProjets = document.getElementById("projets");
-    mesProjets.style.display = "none";
-
-    const sectionPortfolio = document.getElementById("portfolio")
-    sectionPortfolio.insertAdjacentElement("beforebegin", mode);
-
-    // Not display filterBar
-    const filterBar = document.querySelector(".filterbar");
-    filterBar.style.display = "none";
-
-    const modifyButton = document.querySelector(".modify");
-    modifyButton.addEventListener("click", (e) => {
-      // displayModal(modal);
-      generateModal(works);
-    });
-  }
+  // Not display filterBar
+  const filterBar = document.querySelector(".filterbar");
+  filterBar.style.display = "none";
 }
-// execute function
-verifyIfUserLogin()
